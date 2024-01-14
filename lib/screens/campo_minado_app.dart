d54339bdf4658f9c56f1ac76a31d8c02240d8621
@Souza83 <tec.psouza@gmail.com>
2024-01-14 08:50:16 -0300

    Calcular tamanho do tabuleiro.

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -1,10 +1,9 @@
 import 'package:campo_minado/components/tabuleiro_widget.dart';
+import 'package:campo_minado/models/explosao_exception.dart';
 import 'package:campo_minado/models/tabuleiro.dart';
 import 'package:flutter/material.dart';
 import 'package:campo_minado/components/resultado_widget.dart';
-import '../components/campo_widget.dart';
 import '../models/campo.dart';
-import '../models/explosao_exception.dart';
 
 class CampoMinadoApp extends StatefulWidget {
   const CampoMinadoApp({super.key});
@@ -15,16 +14,12 @@ class CampoMinadoApp extends StatefulWidget {
 
 class _CampoMinadoAppState extends State<CampoMinadoApp> {
   bool? _venceu;
-  Tabuleiro _tabuleiro = Tabuleiro(
-    linhas: 12,
-    colunas: 12,
-    qtdeBombas: 3,
-  );
+  Tabuleiro? _tabuleiro;
 
   void _reiniciar() {
     setState(() {
       _venceu = null;
-      _tabuleiro.reiniciar();
+      _tabuleiro!.reiniciar();
     });
   }
 
@@ -32,15 +27,16 @@ class _CampoMinadoAppState extends State<CampoMinadoApp> {
     if (_venceu != null) {
       return;
     }
+
     setState(() {
       try {
         campo.abrir();
-        if (_tabuleiro.resolvido) {
+        if (_tabuleiro!.resolvido) {
           _venceu = true;
         }
       } on ExplosaoException {
         _venceu = false;
-        _tabuleiro.revelarBombas();
+        _tabuleiro!.revelarBombas();
       }
     });
   }
@@ -52,27 +48,51 @@ class _CampoMinadoAppState extends State<CampoMinadoApp> {
 
     setState(() {
       campo.alternarMarcacao();
-      if (_tabuleiro.resolvido) {
+      if (_tabuleiro!.resolvido) {
         _venceu = true;
       }
     });
+  }
+
+  Tabuleiro _getTabuleiro(double largura, double altura) {
+    if (_tabuleiro == null) {
+      int qtdeColunas = 15;
+      double tamanhoCampo = largura / qtdeColunas;
+      int qtdeLinhas = (altura / tamanhoCampo).floor();
+
+      _tabuleiro = Tabuleiro(
+        linhas: qtdeLinhas,
+        colunas: qtdeColunas,
+        qtdeBombas: 50,
+      );
+    }
+    return _tabuleiro!;
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: ResultadoWidget(
-            venceu: _venceu,
-            onReiniciar: _reiniciar,
+        appBar: ResultadoWidget(
+          venceu: _venceu,
+          onReiniciar: _reiniciar,
+        ),
+        body: Container(
+          color: Colors.grey,
+          child: LayoutBuilder(
+            builder: (ctx, constraints) {
+              return TabuleiroWidget(
+                tabuleiro: _getTabuleiro(
+                  constraints.maxWidth,
+                  constraints.maxHeight,
+                ),
+                onAbrir: _abrir,
+                onAlternarMarcacao: _alternarMarcacao,
+              );
+            },
           ),
-          body: Container(
-            child: TabuleiroWidget(
-              tabuleiro: _tabuleiro,
-              onAbrir: _abrir,
-              onAlternarMarcacao: _alternarMarcacao,
-            ),
-          )),
+        ),
+      ),
     );
   }
 }
