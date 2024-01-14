09509b42b9bc12df547d1fb8316f06657e5abda4
@Souza83 <tec.psouza@gmail.com>
2024-01-14 07:00:23 -0300

    Componente Stateful.

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -6,8 +6,20 @@ import '../components/campo_widget.dart';
 import '../models/campo.dart';
 import '../models/explosao_exception.dart';
 
-class CampoMinadoApp extends StatelessWidget {
+class CampoMinadoApp extends StatefulWidget {
   const CampoMinadoApp({super.key});
+
+  @override
+  State<CampoMinadoApp> createState() => _CampoMinadoAppState();
+}
+
+class _CampoMinadoAppState extends State<CampoMinadoApp> {
+  bool? _venceu;
+  Tabuleiro _tabuleiro = Tabuleiro(
+    linhas: 12,
+    colunas: 12,
+    qtdeBombas: 3,
+  );
 
   void _reiniciar() {
     print('reiniciar...');
@@ -26,16 +38,12 @@ class CampoMinadoApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
           appBar: ResultadoWidget(
-            venceu: false,
+            venceu: _venceu,
             onReiniciar: _reiniciar,
           ),
           body: Container(
             child: TabuleiroWidget(
-              tabuleiro: Tabuleiro(
-                linhas: 15,
-                colunas: 15,
-                qtdeBombas: 0,
-              ),
+              tabuleiro: _tabuleiro,
               onAbrir: _abrir,
               onAlternarMarcacao: _alternarMarcacao,
             ),
